9b3c05d5e72e18f1b0f3a4a86bd8ea6d1e01207e
Lorenzo <lorenzozar@gmail.com>
2021-09-09 09:11:36 +0300

    initial design

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Gnocchi',
+      'Pasta',
+      'https://cdn.pixabay.com/photo/2015/07/12/18/56/gnocchi-842305__340.jpg'
+    ),
+    new Recipe(
+      'Gnocchi alla Fioretta',
+      'Pasta',
+      'https://cdn.pixabay.com/photo/2020/01/16/14/22/food-4770756__340.jpg'
+    ),
+    new Recipe(
+      'Gnocchi di Patate',
+      'Pasta',
+      'https://cdn.pixabay.com/photo/2018/03/01/13/04/gnocchi-3190639__340.jpg'
+    ),
+    new Recipe(
+      "Gnocchi all'Anitra",
+      'Pasta',
+      'https://cdn.pixabay.com/photo/2015/03/14/13/34/turkey-673148__340.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 10),
+    new Ingredient('Bread', 2),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
