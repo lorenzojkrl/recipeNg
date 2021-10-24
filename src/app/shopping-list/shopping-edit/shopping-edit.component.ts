ccfdd958747540464506c1501d9ea5f4632c77ef
Lorenzo <lorenzozar@gmail.com>
2021-10-24 20:56:40 +0300

    add item feature

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputReference: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputReference: ElementRef;
+  @Output() ingredientsAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddItem() {
+    const ingredientName = this.nameInputReference.nativeElement.value;
+    const ingredientAmount = this.amountInputReference.nativeElement.value;
+
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+    this.ingredientsAdded.emit(newIngredient);
   }
-
 }
