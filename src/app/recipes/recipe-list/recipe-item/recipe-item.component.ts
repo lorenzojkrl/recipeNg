14f5e5e66018c963dd811ffa8b2ccc0ee9c7fca8
Lorenzo <lorenzozar@gmail.com>
2021-12-22 16:17:16 +0100

    remove service

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,11 +9,7 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onSelect() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
