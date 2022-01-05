0dcf25c02d2be711c71f37213a7ae4da58b4daf4
Lorenzo <lorenzozar@gmail.com>
2022-01-05 23:43:22 +0100

    change EventEmitter to Subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,18 +8,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-
+  private ingChangeSubscription: Subscription;
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
     // subscribe to changes in ingredient list
-    this.shoppingListService.ingredientsChange.subscribe(
-      (ingredientsList: Ingredient[]) => {
-        this.ingredients = ingredientsList;
-      }
-    );
+    this.ingChangeSubscription =
+      this.shoppingListService.ingredientsChange.subscribe(
+        (ingredientsList: Ingredient[]) => {
+          this.ingredients = ingredientsList;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.ingChangeSubscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
-@Injectable({ providedIn: 'root' })
 export class ShoppingListService {
-  ingredientsChange = new EventEmitter<Ingredient[]>(); // inform other Comp that ingredients changed
+  ingredientsChange = new Subject<Ingredient[]>(); // inform other Comp that ingredients changed
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 10),
@@ -16,12 +15,13 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChange.emit(this.ingredients.slice()); // return new array + change
+    // this.ingredientsChange.emit(this.ingredients.slice()); // return new array + change
+    this.ingredientsChange.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients); // add all ingredients, then emit 1 single event.
     //Spread operator turns array of elements into a list which are then pushed
-    this.ingredientsChange.emit(this.ingredients.slice()); // return new array + change
+    this.ingredientsChange.next(this.ingredients.slice()); // return new array + change
   }
 }
