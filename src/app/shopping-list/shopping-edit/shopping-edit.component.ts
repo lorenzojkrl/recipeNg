eb642ecfcf0f6c9eeca4ddbff7d7c841cb5c007d
Lorenzo <lorenzozar@gmail.com>
2022-02-18 08:38:40 +0200

    use template-driven form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,18 +9,20 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputReference: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputReference: ElementRef;
+  // Local ref is not needed when using template driven forms
+  // @ViewChild('nameInput', { static: false }) nameInputReference: ElementRef;
+  // @ViewChild('amountInput', { static: false }) amountInputReference: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
-  onAddItem() {
-    const ingredientName = this.nameInputReference.nativeElement.value;
-    const ingredientAmount = this.amountInputReference.nativeElement.value;
-
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+  onAddItem(f: NgForm) {
+    // Not using local ref
+    // const ingredientName = this.nameInputReference.nativeElement.value;
+    // const ingredientAmount = this.amountInputReference.nativeElement.value;
+    const value = f.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
 
     this.shoppingListService.addIngredient(newIngredient);
   }
