bdd163baba050f0b1e44d100f68bcda2cfe669af
Lorenzo <lorenzozar@gmail.com>
2022-05-25 17:49:53 +0300

    add save data feature

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   collapsed = true;
 
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
-  ngOnInit(): void {}
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
